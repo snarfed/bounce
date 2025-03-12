378117119dc65c74b437d9235b35f3972960ebab
Ryan Barrett <git@ryanb.org>
2025-03-12 15:11:44 -0700

    more unit tests

--- tests/test_pages.py
+++ tests/test_pages.py
@@ -1,4 +1,10 @@
 """Unit tests for pages.py."""
+import json
+
+from flask import get_flashed_messages, session
+from oauth_dropins.bluesky import BlueskyAuth
+from oauth_dropins.mastodon import MastodonAuth
+from oauth_dropins.views import LOGINS_SESSION_KEY
 from oauth_dropins.webutil import flask_util, testutil, util
 from oauth_dropins.webutil.appengine_config import ndb_client
 from oauth_dropins.webutil.testutil import TestCase
@@ -32,3 +38,44 @@ class PagesTest(TestCase):
         # just check that we serve ok
         got = self.client.get('/')
         self.assert_equals(200, got.status_code)
+
+    def test_logout(self):
+        with self.client.session_transaction() as sess:
+            sess[LOGINS_SESSION_KEY] = [('BlueskyAuth', 'did:abc')]
+
+        resp = self.client.post('/logout')
+        self.assertEqual(302, resp.status_code)
+        self.assertEqual('/', resp.headers['Location'])
+        self.assertNotIn(LOGINS_SESSION_KEY, session)
+
+    def test_accounts_no_logins(self):
+        resp = self.client.get('/accounts')
+        self.assertEqual(302, resp.status_code)
+        self.assertEqual('/', resp.headers['Location'])
+
+    def test_accounts(self):
+        BlueskyAuth(id='did:plc:abc', user_json=json.dumps({
+            'handle': 'abc.xyz',
+            'avatar': 'http://abc/pic',
+        })).put()
+        MastodonAuth(id='@a@b.c', access_token_str='', user_json=json.dumps({
+            'uri':'http://b.c/a',
+            'avatar_static': 'http://b.c/pic',
+        })).put()
+
+        with self.client.session_transaction() as sess:
+            sess[LOGINS_SESSION_KEY] = [
+                ('BlueskyAuth', 'did:plc:abc'),
+                ('MastodonAuth', '@a@b.c'),
+            ]
+
+        resp = self.client.get('/accounts')
+        self.assertEqual(200, resp.status_code)
+
+        body = resp.get_data(as_text=True)
+        self.assert_multiline_in("""\
+<img src="http://abc/pic" class="profile">
+<span style="unicode-bidi: isolate">abc.xyz</span>""", body)
+        self.assert_multiline_in("""\
+<img src="http://b.c/pic" class="profile">
+<span style="unicode-bidi: isolate">@a@b.c</span>""", body)
